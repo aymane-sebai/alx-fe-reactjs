f1863c5a11ae068e8d091ff64d49ee215c2b71b4
aymane <aymansebai05@gmail.com>
2025-02-23 23:27:31 +0000

    Fixed App.jsx and integrated WelcomeMessage component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -18,7 +18,18 @@ function App() {
         </a>
       </div>
 
-      {/*  src/components/WelcomeMessage.jsx */}
+      function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
+
       <WelcomeMessage />
 
       <h1>Vite + React</h1>
