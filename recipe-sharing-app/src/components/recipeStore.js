ab233ec338f59ba6664a06b8762ad6870469f027
aymane-sebai <aymansebai05@gmail.com>
2025-03-06 01:37:30 +0100

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,23 +1,9 @@
-import useRecipeStore from '../store/recipeStore';
+import { create } from "zustand";
 
-const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+  setRecipes: (recipes) => set(() => ({ recipes })),
+}));
 
-  return (
-    <div>
-      <h2>Recipe List</h2>
-      {recipes.length === 0 ? (
-        <p>No recipes available. Add one!</p>
-      ) : (
-        recipes.map((recipe) => (
-          <div key={recipe.id} style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))
-      )}
-    </div>
-  );
-};
-
-export default RecipeList;
+export default useRecipeStore;
