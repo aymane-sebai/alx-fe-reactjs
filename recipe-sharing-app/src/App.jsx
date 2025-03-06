a0c5dc6f817aee9a1ea2cca33bac0ab6e0179f03
aymane-sebai <aymansebai05@gmail.com>
2025-03-06 04:46:38 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import SearchBar from './components/SearchBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import React Router components
 import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <RecipeList />
-    </div>
-  );
-}
-
-export default App;
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />  {/* Search functionality */}
+        <Routes>
+          <Rou
