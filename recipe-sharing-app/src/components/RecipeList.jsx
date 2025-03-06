20b1e65bf6d72509ef6c77659154e9b3e8a79a08
aymane-sebai <aymansebai05@gmail.com>
2025-03-06 05:06:06 +0100

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link
+import { Link } from 'react-router-dom';
 import useRecipeStore from '../store/recipeStore';
+import FavoriteButton from './FavoriteButton';
 
 const RecipeList = () => {
-  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  const recipes = useRecipeStore(state => state.recipes);
 
   return (
     <div>
-      <h2>Recipe List</h2>
-      {filteredRecipes.length > 0 ? (
-        filteredRecipes.map((recipe) => (
+      <h2>Recipe List 📜</h2>
+      {recipes.length > 0 ? (
+        recipes.map(recipe => (
           <div key={recipe.id} className="recipe-card">
-            <h3>{recip
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+            <FavoriteButton recipeId={recipe.id} />
+            <Link to={`/recipe/${recipe.id}`}>View Recipe</Link>
+          </div>
+        ))
+      ) : (
+        <p>No recipes found.</p>
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
