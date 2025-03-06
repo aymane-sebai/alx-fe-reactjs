cd800bfe07a48ab3f83383260dc237c964ea953a
aymane-sebai <aymansebai05@gmail.com>
2025-03-06 04:39:23 +0100

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,21 +1,23 @@
-import { create } from "zustand";
+import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) =>
-    set((state) => ({ recipes: [...state.recipes, { id: Date.now(), ...recipe }] })),
+  searchTerm: '',
+  filteredRecipes: [],
+  
+  // Action to set the search term
+  setSearchTerm: (term) => set((state) => {
+    const filtered = state.recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(term.toLowerCase())
+    );
+    return { searchTerm: term, filteredRecipes: filtered };
+  }),
 
-  updateRecipe: (id, updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
-      ),
-    })),
-
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+  // Action to add a new recipe
+  addRecipe: (recipe) => set((state) => ({
+    recipes: [...state.recipes, recipe],
+    filteredRecipes: [...state.filteredRecipes, recipe]
+  })),
 }));
 
 export default useRecipeStore;
