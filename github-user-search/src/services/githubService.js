32ba0e76e42c82d6830c12fe6cd54d42d2ef3b80
aymane-sebai <aymansebai05@gmail.com>
2025-03-30 07:51:29 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
-const GITHUB_API_URL = "https://api.github.com/users/";
+const BASE_URL = "https://api.github.com/search/users";
 
-export const fetchUserData = async (username) => {
+export const fetchAdvancedUsers = async (username, location, minRepos) => {
   try {
-    const response = await axios.get(`${GITHUB_API_URL}${username}`);
-    return response.data;
+    let query = "";
+    if (username) query += `+${username} in:login`;
+    if (location) query += `+location:${location}`;
+    if (minRepos) query += `+repos:>${minRepos}`;
+
+    const response = await axios.get(`${BASE_URL}?q=${query}&per_page=10`);
+    return response.data.items;
   } catch (error) {
-    console.error("Error fetching user:", error);
-    return null;
+    console.error("Error fetching GitHub users:", error);
+    return [];
   }
 };
